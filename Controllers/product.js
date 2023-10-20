affc92b85107b807428a7a9580f8556c00ffa049
aphinan <apinun16159@gmail.com>
2023-10-20 22:39:55 +0700

    update_method

--- Controllers/product.js
+++ Controllers/product.js
@@ -4,53 +4,55 @@ const apiUrl =
 axios.defaults.headers.common["api-key"] = "gihuFSYfpM8d3240k2ZS7J4z236g84";
 
 exports.read = async (req, res) => {
-  const device_id = req.params.device_id;
   try {
-    const response = await axios
-      .get(`${apiUrl}/devicelist/${device_id}`);
-    res.json(response.data);
+    const id = req.params.id;
+    const response = await axios.get(apiUrl + "/devicelist/");
+    const producted = response.data.find((item) => item.device_id === id);
+
+    if (producted) {
+      res.send(producted);
+    } else {
+      res.status(404).json({ message: "Record not found" });
+    }
   } catch (error) {
-    res.status(404).json({ message: "Record not found" });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
 exports.list = async (req, res) => {
   try {
-    const response = await axios.get(`${apiUrl}/devicelist`);
-    res.json(response.data);
+    const response = await axios.get(apiUrl + "/devicelist");
+    res.send(response.data);
   } catch (error) {
-    res.status(500).json({ message: "Failed to retrieve records" });
+    res.status(500).json({ message: "Failed to list records" });
   }
 };
+
 exports.create = async (req, res) => {
   try {
-    const response = await axios.post(`${apiUrl}/Device`, req.body);
-    res.json(response.data);
+    const response = await axios.post(apiUrl + "/Device", req.body);
+    res.send(response.data);
   } catch (error) {
     res.status(500).json({ message: "Failed to create record" });
   }
 };
+
 exports.update = async (req, res) => {
-  const device_id = req.params.device_id;
   try {
-    const response = await axios.put(
-      `${apiUrl}/UpdateDevice/${device_id}`,
-      req.body
-    );
-    res.json(response.data);
+    const response = await axios.put(apiUrl + "/UpdateDevice", req.body, {
+      new: true,
+    });
+    res.send(response.data);
   } catch (error) {
-    res.status(500).json({ message: "Failed to update record" });
+    res.status(500).json({ message: "Failed to update records" });
   }
 };
+
 exports.remove = async (req, res) => {
   try {
-    const device_id = req.params.device_id;
-    const response = await axios.delete(
-      `${apiUrl}/UpdateDevice/${device_id}`,
-      req.body
-    );
-    res.json(response.data);
+    const response = await axios.put(apiUrl + "/DeleteDevice", req.body);
+    res.send(response.data);
   } catch (error) {
-    res.status(500).json({ message: "Failed to delete record" });
+    res.status(500).json({ message: "Failed to Delete records" });
   }
 };
